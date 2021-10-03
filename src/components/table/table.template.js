2630137e9ba7236e2b4a93508929b039941a2e13
Molchanova Tatyana <mtuspeh@gmail.com>
2021-10-03 13:45:40 +0300

    start creating Table

---
+++ src/components/table/table.template.js
@@ -0,0 +1,61 @@
+const CODES = {
+    A: 65,
+    Z: 90
+}
+
+// function createCell() {
+//     return `
+//          <div class="cell" contenteditable>B2</div>
+//     `
+// }
+
+function toColumn(col) {
+    return `
+        <div class="column">${col}</div>
+    `
+}
+
+function createRow(content) {
+    return `
+        <div class="row">
+            <div class="row-info"></div>
+            <div class="row-data">${content}</div>
+        </div>
+    `
+}
+
+// _ symbol is used for unused parameter
+function toChar(_, index) {
+    return String.fromCharCode(CODES.A + index)
+}
+
+export function createTable(rowsCount = 25) {
+    const colsCount = CODES.Z - CODES.A + 1
+    const rows = []
+
+    const cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(toColumn)
+        .join('')
+
+    // const cols = new Array(colsCount)
+    //     .fill('')
+    //     .map((el, index) => {
+    //         return String.fromCharCode(CODES.A + index)
+    //     })
+    //     .map(el => {
+    //         return createCol(el)
+    //     })
+    //     .join('')
+
+    console.log(cols)
+
+    rows.push(createRow(cols))
+
+    for (let i = 0; i < rowsCount; i++) {
+        rows.push(createRow())
+    }
+
+    return rows.join('')
+}
