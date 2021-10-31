8252a7baaf3dde207f80a47fc199387d5368fffd
Tatyana Molchanova <44355525+TatyanaMolchanova@users.noreply.github.com>
2021-10-31 10:56:36 +0200

    Routing implemented (#6)
    Co-authored-by: Molchanova Tatyana <mtuspeh@gmail.com>

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,7 +3,8 @@ import {
     CHANGE_STYLES,
     CHANGE_TEXT,
     CHANGE_TITLE,
-    TABLE_RESIZE
+    TABLE_RESIZE,
+    UPDATE_DATE
 } from './types';
 
 // Action Creator
@@ -18,6 +19,12 @@ export function changeText(data) {
     return {
         type: CHANGE_TEXT,
         data
+    }
+}
+
+export function updateDate() {
+    return {
+        type: UPDATE_DATE
     }
 }
 
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,7 +5,7 @@ import {
     CHANGE_STYLES,
     CHANGE_TEXT,
     CHANGE_TITLE,
-    TABLE_RESIZE
+    TABLE_RESIZE, UPDATE_DATE
 } from './types';
 // import {toInlineStyles} from '../core/utils';
 
@@ -55,6 +55,8 @@ export function rootReducer(state, action) {
             }
         case CHANGE_TITLE:
             return {...state, title: action.data}
+        case UPDATE_DATE:
+            return {...state, openedDate: new Date().toJSON()}
         default: return state
     }
 }
